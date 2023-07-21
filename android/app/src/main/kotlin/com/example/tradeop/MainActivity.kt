e41c060706dc0825e0f8a89b6cad30e076801987
scriptscrypt <scriptscrypt@users.noreply.github.com>
2023-07-21 12:18:54 +0000

    deploy: 5c9cfc1b631a6efbe81f9e35ec120b1908b83f5f

--- android/app/src/main/kotlin/com/example/tradeop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tradeop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/tradex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.foresee
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
